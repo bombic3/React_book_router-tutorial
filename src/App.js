fe030b160f4bdc1b346480d0bb0ea1a4714f11f0
bombi <bombic3@naver.com>
2022-07-11 16:43:50 +0900

    FEAT : NotFound 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Articles from './pages/Articles';
 import Article from './pages/Article';
+import NotFound from './pages/NotFound';
 
 // index props는 path=” / ”와 동일한 의미
 // 좀 더 명시적으로 표현
@@ -18,8 +19,9 @@ function App() {
         <Route path="/profiles/:username" element={<Profile />} />
       </Route>
       <Route path="/articles/" element={<Articles />}>
-        <Route path="/articles/:id" element={<Article />} />
+        <Route path=":id" element={<Article />} />
       </Route>
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
