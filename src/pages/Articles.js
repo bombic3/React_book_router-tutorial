84a58d2d4998e3c791d59c995b8c58e74077d003
bombi <bombic3@naver.com>
2022-07-11 16:30:42 +0900

    STYLE : NavLink 컴포넌트 사용하여 스타일 지정

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,19 +1,42 @@
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 const Articles = () => {
+  const activeStyle = {
+    color: 'green',
+    fontSize: 21,
+  };
+
   return (
     <div>
       <Outlet />
       {/* Outlet 컴포넌트가 사용된 자리에 중첩된 라우트가 보여지게 됨 */}
       <ul>
         <li>
-          <Link to="/articles/1">게시글 1</Link>
+          <NavLink
+            to="/articles/1"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글1
+          </NavLink>
+          {/* <Link to="/articles/1">게시글 1</Link> */}
         </li>
         <li>
-          <Link to="/articles/2">게시글 2</Link>
+          <NavLink
+            to="/articles/2"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글2
+          </NavLink>
+          {/* <Link to="/articles/2">게시글 2</Link> */}
         </li>
         <li>
-          <Link to="/articles/3">게시글 3</Link>
+          <NavLink
+            to="/articles/3"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글3
+          </NavLink>
+          {/* <Link to="/articles/3">게시글 3</Link> */}
         </li>
       </ul>
     </div>
