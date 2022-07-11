d0a82688ae737dc96b739903490883316621d89e
bombi <bombic3@naver.com>
2022-07-11 16:37:43 +0900

    REFACTOR : ArticleItem 컴포넌트 만들어 반복코드 최소화

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,45 +1,33 @@
 import { NavLink, Outlet } from 'react-router-dom';
 
 const Articles = () => {
+  return (
+    <div>
+      <Outlet />
+      <ul>
+        <ArticleItem id={1} />
+        <ArticleItem id={2} />
+        <ArticleItem id={3} />
+      </ul>
+    </div>
+  );
+};
+  
+const ArticleItem = ({ id }) => {
   const activeStyle = {
     color: 'green',
     fontSize: 21,
   };
 
   return (
-    <div>
-      <Outlet />
-      {/* Outlet 컴포넌트가 사용된 자리에 중첩된 라우트가 보여지게 됨 */}
-      <ul>
-        <li>
-          <NavLink
-            to="/articles/1"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            게시글1
-          </NavLink>
-          {/* <Link to="/articles/1">게시글 1</Link> */}
-        </li>
-        <li>
-          <NavLink
-            to="/articles/2"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            게시글2
-          </NavLink>
-          {/* <Link to="/articles/2">게시글 2</Link> */}
-        </li>
-        <li>
-          <NavLink
-            to="/articles/3"
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            게시글3
-          </NavLink>
-          {/* <Link to="/articles/3">게시글 3</Link> */}
-        </li>
-      </ul>
-    </div>
+    <li>
+      <NavLink
+        to={`/articles/${id}`}
+        style={({ isActive }) => (isActive ? activeStyle : undefined)}
+      >
+        게시글 {id}
+      </NavLink>
+    </li>
   );
 };
 
